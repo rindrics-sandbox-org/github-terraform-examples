321dfac3e64658a4061b16bfe9e28fda3fca7a6f
Akira Hayashi <rindrics@gmail.com>
2024-11-20 04:37:29 +0900

    Assign user to teams

--- all-in-one-state.tf
+++ all-in-one-state.tf
@@ -11,3 +11,11 @@ resource "github_team" "all_in_one" {
   name    = each.value
   privacy = "closed"
 }
+
+resource "github_team_membership" "all_in_one" {
+  for_each = github_team.all_in_one
+
+  team_id  = each.value.name
+  username = one([for m in github_membership.org_owner : m.username if m.username == "Rindrics"])
+  role     = "maintainer"
+}
