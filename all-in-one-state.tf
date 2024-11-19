8fd8494fc48c1d787aeeaec5803a927a072d6f5d
Akira Hayashi <rindrics@gmail.com>
2024-11-20 04:40:57 +0900

    Invite teams to repos

--- all-in-one-state.tf
+++ all-in-one-state.tf
@@ -19,3 +19,20 @@ resource "github_team_membership" "all_in_one" {
   username = one([for m in github_membership.org_owner : m.username if m.username == "Rindrics"])
   role     = "maintainer"
 }
+
+resource "github_team_repository" "all_in_one" {
+  for_each = {
+    for pair in flatten([
+      for team, repos in var.team_repo_mapping : [
+        for repo in repos : {
+          team = team
+          repo = repo
+        }
+      ]
+    ]) : "${pair.team}:${pair.repo}" => pair
+  }
+
+  team_id    = github_team.all_in_one[each.value.team].id
+  repository = github_repository.all_in_one[each.value.repo].name
+  permission = "maintain"
+}
--- variables.tf
+++ variables.tf
@@ -17,3 +17,14 @@ variable "teams" {
     "team-5",
   ]
 }
+
+variable "team_repo_mapping" {
+  type = map(list(string))
+  default = {
+    "team-1" = ["foo"]
+    "team-2" = ["foo", "baz"]
+    "team-3" = ["foo", "bar", "baz"]
+    "team-4" = ["baz"]
+    "team-5" = ["foo", "bar", "baz"]
+  }
+}
